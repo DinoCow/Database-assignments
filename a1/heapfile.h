d290f9565cca19000d59bc6a0ce6f2355c2af4d5
Yuki Saito <mintyplanet@gmail.com>
2014-09-24 16:34:01 -0400

    Initial commit of headers and empty source files.

---
+++ a1/heapfile.h
@@ -0,0 +1,39 @@
+#include "record.h"
+#include "page.h"
+
+typedef struct {
+    FILE *file_ptr;
+    int page_size;
+} Heapfile;
+
+typedef int PageID;
+
+typedef struct {
+    int page_id;
+    int slot;
+} RecordID;
+
+/**
+ * Initalize a heapfile to use the file and page size given.
+ */
+void init_heapfile(Heapfile *heapfile, int page_size, FILE *file);
+/**
+ * Allocate another page in the heapfile.  This grows the file by a page.
+ */
+PageID alloc_page(Heapfile *heapfile);
+/**
+ * Read a page into memory
+ */
+void read_page(Heapfile *heapfile, PageID pid, Page *page);
+/**
+ * Write a page from memory to disk
+ */
+void write_page(Page *page, Heapfile *heapfile, PageID pid); 
+
+
+class RecordIterator {
+    public:
+    RecordIterator(Heapfile *heapfile);
+    Record next();
+    bool hasNext();
+}
