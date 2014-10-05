b1fde2be1953c8a7b4614248858e30f6f806beba
kng93 <karin.ng@mail.utoronto.ca>
2014-10-05 19:28:03 -0400

    Wrote read_fixed_len_pages Previous commit message was an error - that was a change to write_fixed_len_pages because of incompatibility with 'LINE_MAX'

---
+++ a1/read_fixed_len_pages.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include "record.h"
+#include "page.h"
+
+#include <vector>
+
+using namespace std;
+const int SLOT_SIZE = 1000;	// Size of a record
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3) 
+	{
+		cerr <<
+		    "Usage: read_fixed_len_pages <page_file> <page_size>"
+		    << endl;
+		return (1);
+	}
+	FILE *page_file = fopen(argv[1], "r");
+	int page_size = atoi(argv[2]);
+
+	if (!page_file || !(page_size > 0)) 
+	{
+		cerr << "FAIL" << endl;
+		return (1);
+	}
+
+	Page *page = NULL;
+	char line[page_size];
+
+	// Iterate over each page in the page file
+	while (fread(line, sizeof(char), page_size, page_file)) 
+	{	
+		page = new Page;
+		init_fixed_len_page(page, page_size, SLOT_SIZE);
+		page->data = line;
+
+		Record rec(100);
+		int numRecs = fixed_len_page_capacity(page) - fixed_len_page_freeslots(page);
+		
+		// Iterate over the records
+		for (int i = 0; i < numRecs; i++)
+		{
+			read_fixed_len_page(page, i, &rec);
+
+			// Output the record as a csv
+			for (Record::iterator it = rec.begin(); it!=rec.end() && !rec.empty(); ++it) 
+			{
+				// Append the comma before every element but the first
+				if(it!=rec.begin()) {
+				    cout << ",";
+				}
+
+				cout << *it;
+			}
+			cout << endl;
+		}
+
+		//Free char arrays in record
+		for (int i = 0; i < rec.size(); i++) 
+		{
+			delete[]rec[i];
+		}
+        delete page;
+	}
+
+	fclose(page_file);
+}
