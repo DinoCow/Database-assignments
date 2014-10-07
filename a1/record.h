9c49159d2fcf48974d5eac088183ddfa43109f7a
kng93 <karin.ng@mail.utoronto.ca>
2014-10-07 12:39:15 -0400

    Implemented select - Not testing for compile... - Added missing semi-colon to record.h

--- a1/record.h
+++ a1/record.h
@@ -31,5 +31,5 @@ void print_record_as_csv(Record *record);
 /*
  * Split line on ',' and insert tokens into record
  */
-void load_csv_record(Record * record, char line[])
+void load_csv_record(Record * record, char line[]);
 #endif
--- a1/select.cpp
+++ a1/select.cpp
@@ -1,5 +1,49 @@
-#include <stdio.h>
+#include <cassert>
+#include <fstream>
+#include <cstdlib>
+#include <cstring>
+#include <sstream>
 
-int main(int argc, char** argv) {
-	printf("Implement me!\n");
+#include "record.h"
+#include "page.h"
+#include "heapfile.h"
+
+#include <sys/timeb.h>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	if (argc != 6) {
+		cerr << "Usage: select <heapfile> <attribute_id> "
+		     << "<start> <end> <page_size>"
+		     << endl;
+		return (1);
+	}
+
+	FILE *heapfd = fopen(argv[1], "r");
+	int attribute_id = atoi(argv[2]);
+	int page_size = atoi(argv[5]);
+
+	if (!heapfd || !(page_size > 0) || !(attribute_id > 0)) {
+		cerr << "FAIL" << endl;
+		return (1);
+	}
+
+	Heapfile heapfile;
+	init_heapfile(&heapfile, page_size, heapfd);
+
+    RecordIterator rit(&heapfile);
+
+    while(rit.hasNext()) {
+    	Record rec = rit.next();
+    	if (strcmp(rec[attribute_id], argv[3]) >= 0 && 
+    		strcmp(rec[attribute_id], argv[4]) <= 0)
+    	{
+    		print_record_as_csv(&rec);
+    	}
+    }
+
+	fclose(heapfd);
 }
