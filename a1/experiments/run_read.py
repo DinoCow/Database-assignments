86168155a99392295f2231a0e62a94bca4f73dfa
Kevin <KevinKYuen@gmail.com>
2014-10-13 23:34:45 -0400

    added the experiments

---
+++ a1/experiments/run_read.py
@@ -0,0 +1,38 @@
+from subprocess import Popen, PIPE
+import sys
+import re
+
+filename = "test.csv"
+page_file = "pagefile"
+num_of_rec = 10000
+page_size = (
+    1002,
+    2000,
+    4000,
+    8000,
+    16000,
+    32000,
+    64000,
+    128000,
+    256000,
+    512000
+)
+
+perf_x = []
+perf_y = []
+for sz in page_size:
+    p = Popen(
+        ['../read_fixed_len_pages', page_file, '%d' % sz],
+        stdout=PIPE)
+    s = p.stdout.read().strip()
+    print '>%s' % s
+    perf_y.append( float(re.findall(r'\d+', s)[0]) * num_of_rec / 1000)
+    perf_x.append( sz)
+
+import matplotlib
+from pylab import *
+figure(1)
+loglog(perf_x, perf_y)
+xlabel('page size')
+ylabel('record/s')
+savefig('read.png')
---
+++ a1/experiments/run_select.py
@@ -0,0 +1,41 @@
+from subprocess import Popen, PIPE
+import sys
+import re
+
+heapfile = "heapfile"
+attribute_id = 0;
+start = "start"
+end = "end"
+
+num_of_rec = 10000
+page_size = (
+    1002,
+    2000,
+    4000,
+    8000,
+    16000,
+    32000,
+    64000,
+    128000,
+    256000,
+    512000
+)
+
+perf_x = []
+perf_y = []
+for sz in page_size:
+    p = Popen(
+        ['../select', heapfile, '%d' % attribute_id, start, end , '%d' % sz],
+        stdout=PIPE)
+    s = p.stdout.read().strip()
+    print '>%s' % s
+    perf_y.append( float(re.findall(r'\d+', s)[0]) * num_of_rec / 1000)
+    perf_x.append( sz)
+
+import matplotlib
+from pylab import *
+figure(1)
+loglog(perf_x, perf_y)
+xlabel('page size')
+ylabel('record/s')
+savefig('select.png')
