41ed46f8c392787ab925d3e3a8ebf7abae06bd12
Kevin <KevinKYuen@gmail.com>
2014-10-31 20:11:49 -0400

    created script to generate the data needed for experiments

--- a2/experiment/bsort.py
+++ a2/experiment/bsort.py
@@ -2,6 +2,8 @@ from subprocess import Popen, PIPE
 import sys
 import re
 
+data_dir = "data/"
+schema_file = "../schema_example.json"
 file_size = ( #file_size in terms of # of tuples
     100,
     200,
@@ -17,6 +19,21 @@ file_size = ( #file_size in terms of # of tuples
 
 perf_x = [ 1, 4, 10, 14]
 perf_y = [100, 200, 400, 800]
+
+#pseudo code for now:
+for sz in file_size:
+    data_file = data_dir + str(sz)
+    # p = Popen(
+    #      ['../bsort', schema_file, data_file, <out_index>],
+    #      stdout=PIPE)
+    # s = p.stdout.read().strip()
+    #     print '>%s' % s
+    #     perf_y.append( float(re.findall(r'\d+', s)[0]))
+    #     perf_x.append( sz)
+
+
+
+
 # for sz in page_size:
 #     p = Popen(
 #         ['../csv2colstore', filename, colstore, '%d' % sz],
